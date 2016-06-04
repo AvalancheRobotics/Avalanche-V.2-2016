4706ab832f47caa0f52dbd727dbcb91d2a6fcb7c
austin zhang <austinzhang1018@gmail.com>
2016-06-03 21:27:55 -0400

    Fixed small bug in LinearSlides' manual slides method

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Modules/LinearSlides.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Modules/LinearSlides.java
@@ -6,7 +6,9 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import org.usfirst.ftc.exampleteam.yourcodehere.Height;
 
 /**
- * Created by Austin on 6/3/2016.
+ * Class for controlling linear slides
+ * Call manualSlide in a recursive loop, updating the input
+ * call Retract slides to retract slides to the starting position
  */
 public class LinearSlides {
     DcMotor motor;
@@ -17,25 +19,23 @@ public class LinearSlides {
         motor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
     }
 
-    public void retractSlides(boolean button, Position startPosition) {
-        if (button) {
+    public void retractSlides(Position startPosition) {
             if (!motor.getMode().equals(DcMotorController.RunMode.RUN_TO_POSITION)) {
                 motor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
             }
             motor.setPower(1);
             motor.setTargetPosition(startPosition.getValue());
             runningAutoRetract = true;
-        }
     }
 
     //Stops any auto methods using slides and manually controls power with joysticks
-    public void manualSlide(float leftInput, float rightInput) {
-        if (ScaleInput.scale(leftInput) != 0) { //Threshold so you don't accidentally start running the slides manually
+    public void manualSlide(float input) {
+        if (ScaleInput.scale(input) != 0) { //Threshold so you don't accidentally start running the slides manually
             if (!motor.getMode().equals(DcMotorController.RunMode.RUN_USING_ENCODERS)) {
                 motor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
                 runningAutoRetract = false;
             }
-            motor.setPower(ScaleInput.scale(leftInput));
+            motor.setPower(ScaleInput.scale(input));
         } else {
             if (runningAutoRetract) {
                 if (motor.getCurrentPosition() >= motor.getTargetPosition() - 20 && motor.getCurrentPosition() <= motor.getTargetPosition() + 20) {
